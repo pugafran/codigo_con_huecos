fcbdf6ea8054629a95b45b6e7c924112d6b535f2
pugafran <franciscplgi36@gmail.com>
2023-03-29 19:42:17 +0200

    Validar IP

--- p1/validar_ip.c
+++ p1/validar_ip.c
@@ -2,15 +2,34 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 
-int main(int argc, char *argv[]){
-    struct sockaddr_in sa;
-    char *ip = argv[1];
-    int result = inet_pton(AF_INET, ip, &(sa.sin_addr));
-    if (result != 1) {
-        // No es una dirección IP válida
-        printf("\nResult = %d",result);
+int check_ip(const char* ip_address) {
+    struct in_addr addr;
+    
+    // Convertir la dirección IP en formato de cadena a una estructura de in_addr
+    if (inet_pton(AF_INET, ip_address, &addr) <= 0) {
+        printf("%s no es una dirección IP válida.\n", ip_address);
+        return 0;
     }
+    
+    // Verificar si la dirección es una dirección de broadcast, multicast o de red
+    if (addr.s_addr == INADDR_BROADCAST ||
+        (ntohl(addr.s_addr) & 0xff000000) == 0x7f000000 ||
+        (ntohl(addr.s_addr) & 0xf0000000) == 0xe0000000 ||
+        (ntohl(addr.s_addr) & 0xf0000000) == 0xf0000000) {
+        printf("%s es una dirección de broadcast, multicast o de red.\n", ip_address);
+        return 0;
+    }
+    
+    printf("%s es una dirección IP válida.\n", ip_address);
+    return 1;
+}
 
-    printf("\nResult = %d",result);
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s [direccion_ip]\n", argv[0]);
+        return 1;
+    }
+    
+    check_ip(argv[1]);
     return 0;
 }
